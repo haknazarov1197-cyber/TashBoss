f74bcdd760b83edce2a6d046821e198fc3bdd3ce
haknazarov1197-cyber <haknazarov1197@gmail.com>
2025-11-05 14:48:55 +0500

    start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
-# start.sh
-# Запускаем Gunicorn с Uvicorn Worker, который необходим для асинхронного Starlette/FastAPI
-gunicorn api:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT
+#!/usr/bin/env bash
+
+# Установка переменных окружения для логирования
+set -eo pipefail
+
+echo "Starting Gunicorn using 'python -m gunicorn' to ensure the correct Python environment is loaded."
+
+# ИСПОЛЬЗУЕМ: 'python -m gunicorn' вместо просто 'gunicorn'
+# Это гарантирует, что мы используем Python-интерпретатор,
+# который установил все зависимости, включая 'firebase-admin'.
+/usr/bin/env python -m gunicorn api:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:"$PORT"
